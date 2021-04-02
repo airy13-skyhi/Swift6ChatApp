b0bda3f684825ce824a12d3a8f8cbbc5804598af
github airy13-skyhi <airy13@icloud.com>
2021-04-02 20:53:50 +0900

    画像送信Model追加

---
+++ Swift6ChatApp/Model/SendToDBModel.swift
@@ -0,0 +1,20 @@
+//
+//  SendToDBModel.swift
+//  Swift6ChatApp
+//
+//  Created by Manabu Kuramochi on 2021/04/02.
+//
+
+import Foundation
+import Firebase
+
+
+class SendToDBModel {
+    
+    
+    
+    
+    
+}
+
+
